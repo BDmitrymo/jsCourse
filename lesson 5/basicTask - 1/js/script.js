b6a0ff0359efcde6d0ca9d0945635bc887854fe5
Dmitry <dbatkovmo@gmail.com>
2018-06-07 00:17:18 +0300

    поправки 5 урока

--- lesson 5/basicTask - 1/js/script.js
+++ lesson 5/basicTask - 1/js/script.js
@@ -1,8 +1,13 @@
 /* Восстановить порядок в меню, добавить пятый пункт */
-var item = document.getElementsByTagName('li');
 
-item[1].innerHTML = 'Второй пункт';
-item[2].innerHTML = 'Третий пункт';
+var item = document.getElementsByClassName('menu-item'),
+	elem = document.createElement('li');
+	elem.classList.add('menu-item');
+	elem.textContent = 'Пятый пункт';
+
+	item[0].parentNode.insertBefore(item[2], item[1]);
+	item[0].parentNode.insertBefore(elem, item[3]);
+	item[0].parentNode.insertBefore(item[4], item[3]);
 
 /* Заменить картинку заднего фона на другую из папки img */
 document.body.style.background = "url('img/apple-true.jpg') center no-repeat";
@@ -15,12 +20,12 @@ tl.textContent = 'Подлинную';
 var advertising = document.getElementsByClassName('adv');
 advertising[0].remove();
 
-/* Спросить у пользователя отношение к технике apple и записать ответ в поле "prompt" */
+/* Спросить у пользователя отношение к технике apple и записать ответ в поле "prompt" 
 var sqr = prompt('Какое отношение к технике apple у Вас', ''),
 	pmpt = document.getElementById('prompt');
 
 	pmpt.innerHTML = sqr;
-
+*/
 
 
 
--- lesson 5/basicTask - 2/js/script.js
+++ lesson 5/basicTask - 2/js/script.js
@@ -2,48 +2,27 @@ var openBtn = document.getElementById('open-btn');
 
 var	nameX = document.getElementsByClassName('name'),
 	name_value = document.getElementsByClassName('name-value'), 
-	budget = document.getElementsByClassName('budget'),
-	budget_value = document.getElementsByClassName('budget-value'),
-	goods = document.getElementsByClassName('goods'),
-	goods_value = document.getElementsByClassName('goods-value'),
-	items = document.getElementsByClassName('items'),
-	items_value = document.getElementsByClassName('items-value'),
+	budgetX = document.getElementsByClassName('budget'),
+	budgetValue = document.getElementsByClassName('budget-value'),
+	goodsX = document.getElementsByClassName('goods'),
+	goodsValue = document.getElementsByClassName('goods-value'),
+	itemsX = document.getElementsByClassName('items'),
+	itemsValue = document.getElementsByClassName('items-value'),
 	employers = document.getElementsByClassName('employers'),
-	employers_value = document.getElementsByClassName('employers-value'),
-	discount = document.getElementsByClassName('discount'),
-	discount_valuev = document.getElementsByClassName('discount-value'),
-	isopen = document.getElementsByClassName('isopen'),
-	isopen_value = document.getElementsByClassName('isopen-value');
+	employersValue = document.getElementsByClassName('employers-value'),
+	discountX = document.getElementsByClassName('discount'),
+	discountValue = document.getElementsByClassName('discount-value'),
+	isopenX = document.getElementsByClassName('isopen'),
+	isopenValue = document.getElementsByClassName('isopen-value');
 
-	console.log(nameX);
-	console.log(name_value);
-	console.log(budget);
-	console.log(budget_value);
-	console.log(goods);
-	console.log(goods_value);
-	console.log(items);
-	console.log(items_value);
-	console.log(employers);
-	console.log(employers_value);
-	console.log(discount);
-	console.log(discount_valuev);
-	console.log(isopen);
-	console.log(isopen_value);
 
 var goodsTtem = document.getElementsByClassName('goods-item');
 
-	console.log(goodsTtem[0]);
-	console.log(goodsTtem[1]);
-	console.log(goodsTtem[2]);
-	console.log(goodsTtem[3]);
-
 var btn = document.getElementsByTagName('button');
+	goodsItemBtn = btn[0];
+	countBudgetBtn = btn[1];
+	hireEmployersBtn = btn[2];
 	
-	console.log(btn[1]);
-	console.log(btn[2]);
-	console.log(btn[3]);
-
-
 var	goods_1 = document.querySelector('#goods_1'),
 	goods_2 = document.querySelector('#goods_2'),
 	goods_3 = document.querySelector('#goods_3'),
@@ -51,18 +30,8 @@ var	goods_1 = document.querySelector('#goods_1'),
 	time = document.querySelector('#time'),
 	budgetX = document.querySelector('#budget');
 
-	console.log(goods_1);
-	console.log(goods_2);
-	console.log(goods_3);
-	console.log(goods_4);
-	console.log(time);
-	console.log(budgetX);
-
-
 var	hireEmployersItem = document.querySelectorAll('.hire-employers-item');
-	console.log(hireEmployersItem[0]);
-	console.log(hireEmployersItem[1]);
-	console.log(hireEmployersItem[2]);
+
 
 
 	
