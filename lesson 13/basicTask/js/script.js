516f1180162fee16b43c6a84184dff5e056b4058
Dmitry <dbatkovmo@gmail.com>
2018-06-20 22:49:18 +0300

    обновление

--- lesson 13/basicTask/js/script.js
+++ lesson 13/basicTask/js/script.js
@@ -12,13 +12,14 @@ $(document).ready(function(){
 		$('.modal').animate({
 			margin: "-15rem 15% 0 18%",
 			width: '70%',
+			height: '50%',
 			marginTop: '80px'
 		}, 1500);
 	});
 
 
 	$('.close').on('click', function() {
-		$('.overlay').fadeTo(3000, 0).fadeOut(200);
+		$('.overlay').fadeTo(2000, 0).fadeOut(200);
 		$('.modal').animate({marginTop: '0px',height: '0'}, 1500).fadeOut(100);	
 	});
 
