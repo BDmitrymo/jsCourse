d91233926b40d437617cf01044b08b9bafecf2a5
Dmitry <dbatkovmo@gmail.com>
2018-06-20 21:41:26 +0300

    обновление

--- lesson 13/basicTask/js/script.js
+++ lesson 13/basicTask/js/script.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
 		$('.overlay').fadeTo(2000, 1.5);
 		$('.modal').css('display','block');
 		$('.modal').animate({
+			margin: "-15rem 15% 0 18%",
 			width: '70%',
 			marginTop: '80px'
 		}, 1500);
