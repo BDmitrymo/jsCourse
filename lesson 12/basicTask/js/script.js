d41496fe62b5944d1b1b7dbcc1c44baa40033af5
Dmitry <dbatkovmo@gmail.com>
2018-06-20 20:03:07 +0300

    обновление

--- lesson 12/basicTask/js/script.js
+++ lesson 12/basicTask/js/script.js
@@ -279,7 +279,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
 	persons.onkeypress = function(event) {   
 		// делаем запреженный список символов
-		let characters = "+.,e";
+		let characters = "+.,?e";
 
 		// записали в перпеменную событие, нажав на символ
 		let key = String.fromCharCode(event.which);
@@ -293,7 +293,7 @@ window.addEventListener('DOMContentLoaded', function() {
 	};
 
 	restDays.onkeypress = function(event) {   
-		let characters = "+.,e",
+		let characters = "+.,?e",
 		    key = String.fromCharCode(event.which);
    
 		if(characters.indexOf(key) >= 0){
@@ -304,30 +304,29 @@ window.addEventListener('DOMContentLoaded', function() {
 		} 
 	};
 
-	persons.addEventListener('change', function(){
-		personsSum = +this.value;
 
- 		if (restDays.value == '') {
+	function testInput() {
+		if (restDays.value == '' || persons.value == '') {
 			totalValue.innerHTML = 0;
  		} else {
- 			total += (daySum + personsSum)*4000;
+ 			total = (daySum + personsSum)*4000;
  			totalValue.innerHTML = total;
+ 			persons.value = personsSum;
+			restDays.value = daySum;
  		}
+	}
+
+	persons.addEventListener('change', function(){
+		personsSum = parseInt(this.value, 10);
+		testInput();
 	});
 
 	restDays.addEventListener('change', function(){
-		daySum = +this.value;
-
-		if (persons.value == '') {
-			totalValue.innerHTML = 0;
- 		} else {
- 			total += (daySum + personsSum)*4000;
- 			totalValue.innerHTML = total;
- 		}
+		daySum = parseInt(this.value, 10);
+		testInput();
 	});
 
 	place.addEventListener('change', function () {
-
 		if (restDays.value == '' || persons.value == '') {
 			totalValue.innerHTML = 0;
 		} else {
